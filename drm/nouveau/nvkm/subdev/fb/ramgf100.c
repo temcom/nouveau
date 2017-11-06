e8ad8e699be397efa70cba6a7208c7b7bc53d70d
Ben Skeggs <bskeggs@redhat.com>
2017-11-06 14:58:59 +1000

    fb/ram/gf10x: timing_10_0b_3f
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drm/nouveau/nvkm/subdev/fb/ramgf100.c
+++ drm/nouveau/nvkm/subdev/fb/ramgf100.c
@@ -78,13 +78,14 @@ gf100_ram_calc_timing(struct gf100_ram *ram)
 	}
 	memx_mask(memx, 0x10f290, mask, data);
 
-	data = (c->bios.timing_10_RCDRD & 0x3f) << 14;
+	data = (c->bios.timing_10_RCDWR & 0x3f) << 20 |
+	       (c->bios.timing_10_RCDRD & 0x3f) << 14;
 	if (ram->base.type != NVKM_RAM_TYPE_GDDR5 &&
 	    v->ramcfg_DLLoff && c->bios.ramcfg_DLLoff)
 		data |= (c->bios.timing_10_CL & 0x7f) - 1;
 	else
 		data |= (c->bios.timing_10_CL & 0x7f);
-	mask = 0x000fc07f;
+	mask = 0x03ffc07f;
 	memx_mask(memx, 0x10f294, mask, data);
 
 	if (mask = 0, data = 0, ram->base.type == NVKM_RAM_TYPE_GDDR5) {
